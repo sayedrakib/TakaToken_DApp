f81872d6a9dbd760ebcfd211bbc5482cfc446aec
Kasper <kajumito@student.jyu.fi>
2018-04-16 16:40:45 +0300

    linted/event addition

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  output:{
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  module: {
-    loaders: [
-      { 
-        test: /\.json$/, use: 'json-loader' 
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query:{
-          presets: ['env']
-        }
-      },
-      {
-        test:/\.s?css$/,
-        loader: 'style-loader!css-loader!sass-loader'
-      }
+    entry: ["babel-polyfill", "./src/index.js"],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build")
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.json$/,
+                use: "json-loader"
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["env"]
+                }
+            },
+            {
+                test: /\.s?css$/,
+                loader: "style-loader!css-loader!sass-loader"
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }])
     ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: './src/index.html', to: "index.html" }
-    ])
-  ]
 };
